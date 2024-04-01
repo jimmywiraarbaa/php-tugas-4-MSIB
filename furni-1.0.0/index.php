7cc22b7829ed7a8acf313b47b3cdf007420eda30
bangbangnam <69713060+jimmywiraarbaa@users.noreply.github.com>
2024-04-02 01:25:41 +0700

    tugas 4 php

---
+++ furni-1.0.0/index.php
@@ -0,0 +1,37 @@
+<!-- /*
+* Bootstrap 5
+* Template Name: Furni
+* Template Author: Untree.co
+* Template URI: https://untree.co/
+* License: https://creativecommons.org/licenses/by/3.0/
+*/ -->
+<!doctype html>
+<html lang="en">
+
+<head>
+	<?php
+	include_once './Head/head.php';
+	?>
+	<title>Furni - Modern</title>
+</head>
+
+<body>
+
+	<?php
+	include_once './Header/header.php';
+	include_once './Hero/hero.php';
+	include_once './Content/whyChoose.php';
+	include_once './Content/weHelp.php';
+	include_once './Content/popularProduct.php';
+	include_once './Content/testimonial.php';
+	include_once './Content/blog.php';
+	include_once './Footer/footer.php';
+	?>
+
+
+	<script src="js/bootstrap.bundle.min.js"></script>
+	<script src="js/tiny-slider.js"></script>
+	<script src="js/custom.js"></script>
+</body>
+
+</html>
